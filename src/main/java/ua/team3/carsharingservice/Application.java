a3429e04abae6c4ccd28747fca81f3ba28874c38
reamfox <sashafarion@gmail.com>
2024-05-29 15:36:13 +0300

    Innit commit

---
+++ src/main/java/ua/team3/carsharingservice/Application.java
@@ -0,0 +1,13 @@
+package ua.team3.carsharingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
