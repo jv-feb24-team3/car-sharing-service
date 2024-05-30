9dbda8bd7577eeac1e2ad60a17f3f5ee7b26562e
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-05-30 15:44:04 +0300

    refactor: change field type in requestDto, change parameter in car mapper

--- src/main/java/ua/team3/carsharingservice/dto/CreateCarRequestDto.java
+++ src/main/java/ua/team3/carsharingservice/dto/CreateCarRequestDto.java
@@ -2,11 +2,12 @@ package ua.team3.carsharingservice.dto;
 
 import java.math.BigDecimal;
 import lombok.Data;
+import ua.team3.carsharingservice.model.Car;
 
 @Data
 public class CreateCarRequestDto {
     private String brand;
-    private String type;
+    private Car.CarType type;
     private int inventory;
     private BigDecimal dailyFree;
 }
