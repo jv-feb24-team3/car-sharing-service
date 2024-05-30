df4faf9fa3b5e8ab9bda418080ebc4d9bdf7b32b
Oksana Miazina <oksana.myazina@gmail.com>
2024-05-30 15:37:16 +0300

    feat: add Rental model

---
+++ src/main/java/ua/team3/carsharingservice/model/Rental.java
@@ -0,0 +1,35 @@
+package ua.team3.carsharingservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "rentals")
+@Getter
+@Setter
+public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "rental_date", nullable = false)
+    private LocalDate rentalDate;
+    @Column(name = "return_date", nullable = false)
+    private LocalDate returnDate;
+    @Column(name = "actual_return_date")
+    private LocalDate actualReturnDate;
+    @ManyToOne
+    @JoinColumn(name = "car_id", nullable = false)
+    private Car car;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
