2d01122e6952fce8fa3a3c1b848f29e408c47c2d
reamfox <sashafarion@gmail.com>
2024-05-30 17:10:07 +0300

    feat: add dependency and config for Stripe

---
+++ src/main/java/ua/team3/carsharingservice/config/StripeConfig.java
@@ -0,0 +1,18 @@
+package ua.team3.carsharingservice.config;
+
+import com.stripe.Stripe;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StripeConfig {
+
+    @Value("${stripe.api.key}")
+    private String stripeApiKey;
+
+    @PostConstruct
+    public void init() {
+        Stripe.apiKey = stripeApiKey;
+    }
+}
