88e5409f98ab6b211b52cf7c5db61d0d586aa283
Denys <Eidenn005@gmail.com>
2024-05-30 13:15:49 +0300

    feat: User and Role classes Add User and Role classes Add liquibase scripts for DB

---
+++ src/main/java/model/Role.java
@@ -0,0 +1,34 @@
+package model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    @Enumerated(EnumType.STRING)
+    private RoleName role;
+
+    public enum RoleName {
+        USER,
+        ADMIN
+    }
+}
