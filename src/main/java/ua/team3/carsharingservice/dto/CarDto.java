274598cd0190637f9b1bc3f4b71d0680807ee834
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-05-30 17:53:57 +0300

    refactor: change field type in CarDto class

--- src/main/java/ua/team3/carsharingservice/dto/CarDto.java
+++ src/main/java/ua/team3/carsharingservice/dto/CarDto.java
@@ -2,12 +2,13 @@ package ua.team3.carsharingservice.dto;
 
 import java.math.BigDecimal;
 import lombok.Data;
+import ua.team3.carsharingservice.model.Car;
 
 @Data
 public class CarDto {
     private Long id;
     private String brand;
-    private String type;
+    private Car.CarType type;
     private int inventory;
     private BigDecimal dailyFee;
 }
