7f5c4ba4a96a33d716bbbd2a262bec88a963d89e
reamfox <sashafarion@gmail.com>
2024-05-30 12:18:11 +0300

    feat: add basic structure of Payment entity

---
+++ src/main/java/ua/team3/carsharingservice/model/Payment.java
@@ -0,0 +1,48 @@
+package ua.team3.carsharingservice.model;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "payments")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    @Basic(optional = false)
+    private Status status;
+    @Enumerated(EnumType.STRING)
+    @Basic(optional = false)
+    private Type type;
+    // Rental id one to one
+    @Column(nullable = false)
+    private String sessionUrl;
+    @Column(nullable = false, unique = true)
+    private String sessionId;
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    public enum Status {
+        PENDING,
+        PAID
+    }
+
+    public enum Type {
+        PAYMENT,
+        FINE,
+        EXPIRED
+    }
+}
