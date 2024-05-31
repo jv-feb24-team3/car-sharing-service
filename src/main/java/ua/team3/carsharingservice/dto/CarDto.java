e6d320f40cbbaab876cc473cc851a75dbf640719
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-05-31 12:58:08 +0300

    refactor: change filed type

--- src/main/java/ua/team3/carsharingservice/dto/CarDto.java
+++ src/main/java/ua/team3/carsharingservice/dto/CarDto.java
@@ -2,13 +2,12 @@ package ua.team3.carsharingservice.dto;
 
 import java.math.BigDecimal;
 import lombok.Data;
-import ua.team3.carsharingservice.model.Car;
 
 @Data
 public class CarDto {
     private Long id;
     private String brand;
-    private Car.CarType type;
+    private String type;
     private int inventory;
     private BigDecimal dailyFee;
 }
