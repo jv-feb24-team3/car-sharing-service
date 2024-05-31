14667be559fc556fd6d682da6a6aae907042d389
Denys <Eidenn005@gmail.com>
2024-05-31 08:29:02 +0300

    Feat: Add Authentication service Add User dtos for login

---
+++ src/main/java/ua/team3/carsharingservice/security/AuthenticationService.java
@@ -0,0 +1,25 @@
+package ua.team3.carsharingservice.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import ua.team3.carsharingservice.dto.UserLoginRequestDto;
+import ua.team3.carsharingservice.dto.UserLoginResponseDto;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+
+    public UserLoginResponseDto authenticate(UserLoginRequestDto requestDto) {
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        requestDto.getEmail(), requestDto.getPassword())
+        );
+        String token = jwtUtil.generateToken(authentication.getName());
+        return new UserLoginResponseDto(token);
+    }
+}
