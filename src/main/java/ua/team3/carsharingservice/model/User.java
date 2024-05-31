624084ed8c9fdc8b1dd389eea4be6527c145e59f
Denys <Eidenn005@gmail.com>
2024-05-31 15:02:06 +0300

    chore: remove redundant dependencies change return of UserDetail overrided methods

--- src/main/java/ua/team3/carsharingservice/model/User.java
+++ src/main/java/ua/team3/carsharingservice/model/User.java
@@ -72,21 +72,21 @@ public class User implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return true;
     }
 }
