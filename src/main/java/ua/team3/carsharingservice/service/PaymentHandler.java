2e653a4031e9662e69d010863cc562577dade364
reamfox <sashafarion@gmail.com>
2024-06-02 17:57:41 +0300

    feat: add some more logic

--- src/main/java/ua/team3/carsharingservice/service/PaymentHandler.java
+++ src/main/java/ua/team3/carsharingservice/service/PaymentHandler.java
@@ -1,10 +1,14 @@
 package ua.team3.carsharingservice.service;
 
 import java.math.BigDecimal;
+import java.util.Optional;
+import ua.team3.carsharingservice.model.Payment;
 import ua.team3.carsharingservice.model.Rental;
 
 public interface PaymentHandler {
     long calculateDays(Rental rental);
 
     BigDecimal calculateAmount(BigDecimal dailyFee, long rentalDays);
+
+    boolean canMakePayment(Rental rental, Optional<Payment> optionalPayment);
 }
