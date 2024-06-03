ff1f4a2fda140d0e2e2dd06cc119812fe21d4da3
Oksana Miazina <oksana.myazina@gmail.com>
2024-06-04 00:36:05 +0300

    style: remove extra import

--- src/main/java/ua/team3/carsharingservice/controller/RentalController.java
+++ src/main/java/ua/team3/carsharingservice/controller/RentalController.java
@@ -3,7 +3,6 @@ package ua.team3.carsharingservice.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
