fd7bf567cdbd17e9814340c02b5bbaa14ac18b7a
Mykola Skrypalov <skripalov1@gmail.com>
2024-06-03 17:31:15 +0300

    refactor: rename method, exception, change message of it

--- src/main/java/ua/team3/carsharingservice/exception/NotificationServiceInternalException.java
+++
@@ -1,7 +0,0 @@
-package ua.team3.carsharingservice.exception;
-
-public class NotificationServiceInternalException extends RuntimeException {
-    public NotificationServiceInternalException(String message) {
-        super(message);
-    }
-}
