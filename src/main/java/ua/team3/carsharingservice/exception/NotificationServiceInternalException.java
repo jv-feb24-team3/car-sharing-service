40dae994f325fd8bb5352975030570484f7befa4
Mykola Skrypalov <skripalov1@gmail.com>
2024-06-03 16:18:11 +0300

    feat: add notification service, method sending notification when new rental created

---
+++ src/main/java/ua/team3/carsharingservice/exception/NotificationServiceInternalException.java
@@ -0,0 +1,7 @@
+package ua.team3.carsharingservice.exception;
+
+public class NotificationServiceInternalException extends RuntimeException {
+    public NotificationServiceInternalException(String message) {
+        super(message);
+    }
+}
