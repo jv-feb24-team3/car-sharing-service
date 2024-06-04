697ab316dcc06d7c9b3cfac34a49cb6a897b1c13
Mykola Skrypalov <skripalov1@gmail.com>
2024-06-04 14:05:54 +0300

    feat: add scheduled telegram notification about overdue rentals

--- src/main/java/ua/team3/carsharingservice/telegram/service/NotificationService.java
+++ src/main/java/ua/team3/carsharingservice/telegram/service/NotificationService.java
@@ -5,4 +5,8 @@ import ua.team3.carsharingservice.model.Rental;
 public interface NotificationService {
 
     void sendRentalCreatedNotification(Rental rental);
+
+    void sendOverdueRentalsNotification(Rental rental);
+
+    void sendNoOverdueRentalsNotification();
 }
