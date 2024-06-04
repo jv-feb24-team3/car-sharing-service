160c9a881b61cdefeed1addf2e6db2f49a71247f
Denys <Eidenn005@gmail.com>
2024-06-04 17:25:47 +0300

    test: remove imports

--- src/main/java/ua/team3/carsharingservice/controller/UsersController.java
+++ src/main/java/ua/team3/carsharingservice/controller/UsersController.java
@@ -2,8 +2,6 @@ package ua.team3.carsharingservice.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
