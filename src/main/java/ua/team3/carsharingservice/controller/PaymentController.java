e442f43272365ee0112dfb733d9da537c7d4cf30
reamfox <sashafarion@gmail.com>
2024-06-05 23:30:22 +0300

    fix: fix creation session endpoint

--- src/main/java/ua/team3/carsharingservice/controller/PaymentController.java
+++ src/main/java/ua/team3/carsharingservice/controller/PaymentController.java
@@ -2,15 +2,15 @@ package ua.team3.carsharingservice.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ua.team3.carsharingservice.dto.stripe.payment.PaymentDto;
-import ua.team3.carsharingservice.dto.stripe.payment.PaymentResponseUrlDto;
 import ua.team3.carsharingservice.dto.stripe.session.SessionCreateDto;
 import ua.team3.carsharingservice.model.User;
 import ua.team3.carsharingservice.service.PaymentService;
@@ -54,14 +53,13 @@ public class PaymentController {
 
     @PostMapping
     @PreAuthorize("hasAnyRole('ADMIN','USER')")
-    @ResponseStatus(HttpStatus.OK)
+    @CrossOrigin(origins = {"https://checkout.stripe.com", "http://localhost:8080"}, maxAge = 3600)
     @Operation(summary = "Create payment session",
             description = "Endpoint for creation of payment session")
-    public void createPaymentSession(@RequestBody @Valid SessionCreateDto createDto,
-                                     @AuthenticationPrincipal User user,
-                                     HttpServletResponse response) {
-        PaymentResponseUrlDto responseDto = paymentService.createPaymentSession(createDto, user);
-        response.setHeader(HttpHeaders.LOCATION, responseDto.sessionUrl());
+    public ResponseEntity<String> createPaymentSession(@RequestBody @Valid SessionCreateDto createDto,
+                                     @AuthenticationPrincipal User user) {
+        String sessionUrl = paymentService.createPaymentSession(createDto, user);
+        return ResponseEntity.ok(sessionUrl);
     }
 
     @GetMapping("/success")
@@ -69,11 +67,11 @@ public class PaymentController {
             description = "This endpoint is used by Stripe "
                     + "to redirect the user after a successful payment. "
                     + "It is not intended to be called directly.")
-    public String paymentSuccess(@RequestParam("session_id") String sessionId) {
+    public ResponseEntity<String> paymentSuccess(@RequestParam("session_id") String sessionId) {
         if (paymentService.isPaymentStatusPaid(sessionId)) {
-            return paymentService.returnSuccessMessage();
+            return ResponseEntity.ok(paymentService.returnSuccessMessage());
         }
-        return paymentService.returnCancelMessage();
+        return ResponseEntity.ok(paymentService.returnCancelMessage());
     }
 
     @GetMapping("/cancel")
@@ -81,7 +79,7 @@ public class PaymentController {
             description = "This endpoint is used by Stripe "
                     + "to redirect the user after a cancelled payment. "
                     + "It is not intended to be called directly.")
-    public String paymentCancel() {
-        return paymentService.returnCancelMessage();
+    public ResponseEntity<String> paymentCancel() {
+        return ResponseEntity.ok(paymentService.returnCancelMessage());
     }
 }
--- src/main/java/ua/team3/carsharingservice/service/PaymentService.java
+++ src/main/java/ua/team3/carsharingservice/service/PaymentService.java
@@ -3,13 +3,12 @@ package ua.team3.carsharingservice.service;
 import java.util.List;
 import org.springframework.data.domain.Pageable;
 import ua.team3.carsharingservice.dto.stripe.payment.PaymentDto;
-import ua.team3.carsharingservice.dto.stripe.payment.PaymentResponseUrlDto;
 import ua.team3.carsharingservice.dto.stripe.session.SessionCreateDto;
 import ua.team3.carsharingservice.model.Rental;
 import ua.team3.carsharingservice.model.User;
 
 public interface PaymentService {
-    PaymentResponseUrlDto createPaymentSession(SessionCreateDto createDto, User user);
+    String createPaymentSession(SessionCreateDto createDto, User user);
 
     List<PaymentDto> getPaymentsByUserId(Long userId, Pageable pageable);
 
